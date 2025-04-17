5fe220a89f5a495ba431d2561074ba0e3cf56e22
curtiselfn <curtis.elfn@gmail.com>
2025-04-17 22:53:15 +0800

    adding the onclick functions for numbers

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ let firstOperand = null;
 let seconOperand = null;
 let operator = null;
 let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll('.btn.number');
+
+numberButtons.forEach(button=>{
+    button.addEventListener('click', () =>{
+        const value = button.textContent;
+        //Update the display value;
+        if(display.textContent === "0"){
+            display.textContent = value; // will display the value
+        } else {
+            display.textContent += value;
+        }
+    })
+})
