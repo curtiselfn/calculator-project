268bb1cec2b23777ef45eff72c8fbb3428ea2e79
curtiselfn <curtis.elfn@gmail.com>
2025-04-17 22:19:53 +0800

    adding variables to the JS files

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Connected")
+
+let display = document.getElementById('display');
+let currentInput = '';
+let firstOperand = null;
+let seconOperand = null;
+let operator = null;
+let shouldResetScreen = false;
