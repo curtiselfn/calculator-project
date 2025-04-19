71902efe53d76ed185b86c714bd78f8cac75a8be
curtiselfn <curtis.elfn@gmail.com>
2025-04-19 21:27:21 +0800

    almost done,

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ let firstOperand = null;
 let operator = null;
 let shouldResetScreen = false;
 
+//-- button selector --//
+
+//clear button
+const clearButton = document.querySelector('.btn.clear');
+
+//number button
+const numberButtons = document.querySelectorAll('.btn.number');
+
+//operator button
+const operatorButtons = document.querySelectorAll('.btn.operator');
+
+//decimal button
+const decimalButton = document.querySelector('.btn.decimal');
+
+//equal button
+const equalButton = document.querySelector('.btn.equal');
+
 //update the display with current input
 function updateDisplay() {
     display.textContent = currentInput || "0";
 }
 
-//adding onclick to every number button
-const numberButtons = document.querySelectorAll('.btn.number');
+//clear display function
+
+clearButton.addEventListener('click',clear);
+
+function clear(){
+    currentInput = '';
+    firstOperand = null;
+    operator = null;
+    shouldResetScreen = false;
+    updateDisplay()
+}
+
+//adding onclick to every number button and handle number presses
 
 numberButtons.forEach( button => {
     button.addEventListener('click', () =>{
@@ -28,9 +56,17 @@ numberButtons.forEach( button => {
         
     })
 })
+//handles decimal
+
+decimalButton.addEventListener('click', handleDecimal);
+
+function handleDecimal(){
+    if (currentInput.includes('.')) return;
+    currentInput += ".";
+    updateDisplay();
+}
 
 //onclick for every operator
-const operatorButtons = document.querySelectorAll('.btn.operator');
 
 operatorButtons.forEach( button =>{
     button.addEventListener('click', () =>{
@@ -39,17 +75,43 @@ operatorButtons.forEach( button =>{
     })
 })
 
+//handling operator +-/*
+function handleOperator(value){
+    if (operator !== null && shouldResetScreen === false) evaluate();
+    firstOperand = currentInput;
+    operator = value;
+    shouldResetScreen = true;
+}
 
+//evaluating
+equalButton.addEventListener('click', evaluate)
 
-//clear display function
-
-const clearButton = document.querySelector('.btn.clear');
-clearButton.addEventListener('click',clear);
-
-function clear(){
-    currentInput = '';
-    firstOperand = null;
+function evaluate(){
+    if (operator === null || shouldResetScreen === true) return;
+    const secondOperand = currentInput;
+    const result = operate(operator, firstOperand, secondOperand);
+    currentInput = result;
     operator = null;
-    shouldResetScreen = false;
+    firstOperand = null;
+    shouldResetScreen = true;
     updateDisplay()
+}
+
+//operate
+function operate(operator, a, b) {
+   const numA = Number(a);
+   const numB = Number(b);
+   switch(operator) {
+    case "+" :
+        return numA + numB;
+    case "-" :
+        return numA - numB;
+    case "*":
+        return numA * numB;
+    case "/":
+        if (numB === 0) return "Error";
+        return numA / numB;
+    default:
+        return "Invalid Operator";
+   }
 }
